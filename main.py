c4df1c262232b2f46815a4c977277ae07f0777b1
Valeriia Fedorenko <xaarlox@gmail.com>
2025-05-28 16:31:00 +0300

    feat: added cell states

--- main.py
+++ main.py
@@ -8,13 +8,38 @@ from datetime import timedelta, datetime
 
 
 class WFSim:
-    def __init__(self, f=0.01, p=1e-4, h=16, w=16):
+    def __init__(self, f=0.01, p=1e-4, bedrock=0.005, water=0.05, h=16, w=16):
         self.f = f
         self.p = p
         self.h = h
         self.w = w
+        self.bedrock = bedrock
+        self.water = water
 
-        self.landscape = np.random.randint(1, 2, (self.h, self.w))
+        self.landscape = np.random.randint(0, 2, (self.h, self.w))
+
+        for i in range(self.landscape.shape[0]):
+            for j in range(self.landscape.shape[1]):
+                coef = 5 if self.surf_neighbors_check(i, j, "B") else 1
+                if self.bedrock * coef > np.random.random():
+                    self.landscape[i, j] = -2
+
+                coef = 10 if self.surf_neighbors_check(i, j, "W") else 0.1
+                if self.water * coef > np.random.random():
+                    self.landscape[i, j] = -3
+
+    def surf_neighbors_check(self, idx, jdx, kind='W'):
+        if kind == 'W':
+            value = -2
+        elif kind == 'B':
+            value = -1
+        offsets = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
+        for di, dj in offsets:
+            ni, nj = idx + di, jdx + dj
+            if 0 <= nj < self.w and 0 <= ni < self.h:
+                if self.landscape[ni, nj] == value:
+                    return True
+        return False
 
     def fire_neighbors_check(self, idx, jdx):
         offsets = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
@@ -31,7 +56,7 @@ class WFSim:
         for i in range(self.landscape.shape[0]):
             for j in range(self.landscape.shape[1]):
                 if self.landscape[i, j] == 2:
-                    new_landscape[i, j] = 0
+                    new_landscape[i, j] = -1
                 if self.p > np.random.rand() and self.landscape[i, j] == 0:
                     new_landscape[i, j] = 1
                 if (self.f > np.random.rand() or self.fire_neighbors_check(i, j)) and self.landscape[i, j] == 1:
@@ -40,15 +65,15 @@ class WFSim:
 
 
 def update(frame):
-    Sim.step(frame)
     im.set_data(Sim.landscape)
     ax.axis('off')
+    Sim.step(frame)
     return [im]
 
 
-color_list = ['black', 'forestgreen', 'orange']
+color_list = ['steelblue', 'grey', 'black', 'olivedrab', 'forestgreen', 'orange']
 cmap = colors.ListedColormap(color_list)
-bounds = [0, 1, 2, 3]
+bounds = [-3, -2, -1, 0, 1, 2, 3]
 norm = colors.BoundaryNorm(bounds, cmap.N)
 
 Sim = WFSim(h=64, w=64)
@@ -58,5 +83,5 @@ im = ax.imshow(Sim.landscape, cmap=cmap, norm=norm)
 plt.subplots_adjust(top=1, bottom=0, right=1, left=0, hspace=0, wspace=0)
 
 ani = animation.FuncAnimation(fig, update, frames=80, interval=20)
-ani.save('simple.gif', fps=1.5, savefig_kwargs={'pad_inches': 0})
+ani.save('new_states.gif', fps=1.5, savefig_kwargs={'pad_inches': 0})
 plt.show()
